2e45def21d1c873d13a999dbd4df4939ac6305ab
mgonline86 <mgonline86@yahoo.com>
2022-12-06 17:34:44 +0200

    Update Todos Routes

--- routes/todos.js
+++ routes/todos.js
@@ -6,37 +6,55 @@ const prisma = new PrismaClient()
 
 // Get all to do for specific user
 router
-.get('/', (req, res) => {
-    res.status(200).json({ todos: ["all todos"] })
+.get('/', async(req, res) => {
+    const todos = await prisma.todo.findMany();
+    res.set('Access-Control-Allow-Origin', '*');
+    res.status(200).json({todos})
 })
 // Add new to do for specific user
 .post('/', async(req, res) => {
-    // const { userId, title, content } = req.body
+    const { userId, title, content } = req.body
     console.log(req.body)
-    // const todo = await prisma.todo.create({
-    //     data: {
-    //         title,
-    //         content,
-    //         author: userId,
-    //     },
-    // })
-    res.status(201).json(req.body)
+    const todo = await prisma.todo.create({
+        data: {
+            title,
+            content,
+            author: userId,
+        },
+    })
+    res.status(201).json(todo)
 })
 
 // Get to do information for specific user
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
     const { id } = req.params
-    res.status(200).json({ id })
+    const todo = await prisma.todo.findUnique({
+        where: {
+          id,
+        },
+    })
+    res.status(200).json({ todo })
 })
 // Update an existing to do for specific user
-.put('/:id', (req, res) => {
+.put('/:id', async(req, res) => {
     const { id } = req.params
-    res.status(200).json({ id })
-})
-// Delete an existing to do for specific user
-.delete('/:id', (req, res) => {
-    const { id } = req.params
-    res.status(200).json({ id })
+    const todo = await prisma.todo.update({
+        where: {
+            id,
+        },
+        data: req.body,
+      })
+      res.status(200).json({ todo })
+    })
+    // Delete an existing to do for specific user
+    .delete('/:id', async(req, res) => {
+        const { id } = req.params
+        const todo = await prisma.todo.delete({
+            where: {
+              id,
+            },
+        })
+    res.status(200).json({ todo })
 })
 
 module.exports = router
