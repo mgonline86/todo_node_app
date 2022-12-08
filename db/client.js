87e1ffd465fe8d6872bcd1c2237400002a1718f4
mgonline86 <mgonline86@yahoo.com>
2022-12-08 08:01:33 +0200

    Removing unnecessary (comments)

--- db/client.js
+++ db/client.js
@@ -1,18 +1,6 @@
 // Import the mongoose module
 import mongoose from "mongoose";
 
-// // Set up default mongoose connection
-// const mongoDB = process.env.MONGODB_URL;
-
-
-// constconnect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// // Get the default connection
-// const db = connection;
-
-// // Bind connection to error event (to get notification of connection errors)
-// db.on("error", console.error.bind(console, "MongoDB connection error:"));
-
 const connectMongo = async () => mongoose.connect(process.env.MONGODB_URL);
 
 export default connectMongo;
